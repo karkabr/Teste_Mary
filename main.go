6968030196e720c1bf3170a4cc94b899d3ce662f
Maria C <mcs_nasc@hotmail.com>
2024-07-23 14:44:13 -0300

    força conflito na main

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World (pra não dá azar kkk)!")
+	fmt.Fprintf(w, "Forçar conflito na main!")
 }
 
 func main() {
