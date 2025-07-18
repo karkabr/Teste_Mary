20ffd7c9c7c8074045a504347e393ad9f01e374d
Maria <mcs_nasc@hotmail.com>
2025-07-18 14:14:39 -0300

    teste sh

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World (pra não dá azar kkk)!")
 	fmt.Fprintf(w, "Forçar conflito na main!")
 	fmt.Fprintf(w, "Teste modificação!")
+	fmt.Fprintf(w, "Teste sh!")
 }
 
 func main() {
