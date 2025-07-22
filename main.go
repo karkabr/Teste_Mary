0d4066f3b5ec4cb50eaeaaebc62cc007c1f48d4c
Maria <mcs_nasc@hotmail.com>
2025-07-22 12:06:21 -0300

    teste arquivo

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Teste B -> A!")
 	fmt.Fprintf(w, "Teste B -> A! - Comparando linhas")
 	fmt.Fprintf(w, "Teste B -> A! - Comparando linhas 2")
+	fmt.Fprintf(w, "Teste A -> B! - arquivo")
 }
 
 func main() {
